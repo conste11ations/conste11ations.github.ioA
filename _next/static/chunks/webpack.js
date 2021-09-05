8daf44dd78a0a31f637c02bcaf6693508d211557
Rachel Kat <rachel.kat@koho.ca>
2021-09-05 02:07:34 -0400

    commit

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "188fbe226d883e56e56e";
+/******/ 	var hotCurrentHash = "bdbe9da145971265f965";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
